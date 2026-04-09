f84fe66d3cd1c15a3da955296c0cfcba96633c91
zhangrun <zrun258@gmail.com>
2026-04-09 13:53:57 +0800

    addRuleInfo

--- project/user/main_cm7_0.c
+++ project/user/main_cm7_0.c
@@ -1,46 +1,4 @@
-﻿/*********************************************************************************************************************
-* CYT4BB Opensourec Library 即（ CYT4BB 开源库）是一个基于官方 SDK 接口的第三方开源库
-* Copyright (c) 2022 SEEKFREE 逐飞科技
-*
-* 本文件是 CYT4BB 开源库的一部分
-*
-* CYT4BB 开源库 是免费软件
-* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
-* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
-*
-* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
-* 甚至没有隐含的适销性或适合特定用途的保证
-* 更多细节请参见 GPL
-*
-* 您应该在收到本开源库的同时收到一份 GPL 的副本
-* 如果没有，请参阅<https://www.gnu.org/licenses/>
-*
-* 额外注明：
-* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
-* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
-* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
-* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
-*
-* 文件名称          main_cm7_0
-* 公司名称          成都逐飞科技有限公司
-* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
-* 开发环境          IAR 9.40.1
-* 适用平台          CYT4BB
-* 店铺链接          https://seekfree.taobao.com/
-*
-* 修改记录
-* 日期              作者                备注
-* 2024-1-4       pudding            first version
-********************************************************************************************************************/
-
-#include "zf_common_headfile.h"
-// 打开新的工程或者工程移动了位置务必执行以下操作
-// 第一步 关闭上面所有打开的文件
-// 第二步 project->clean  等待下方进度条走完
-
-// 本例程是开源库空工程 可用作移植或者测试各类内外设
-// 本例程是开源库空工程 可用作移植或者测试各类内外设
-// 本例程是开源库空工程 可用作移植或者测试各类内外设
+﻿#include "zf_common_headfile.h"
 
 // **************************** 代码区域 ****************************
 
@@ -51,41 +9,15 @@ int main(void)
     debug_init();                       // 调试串口信息初始化
     // 此处编写用户代码 例如外设初始化代码等
     balance_cascade_init();              // 串级平衡控制初始化
-    printf("\r\n balance_cascade_init ok.");
     small_driver_uart_init();            // 小车控制串口初始化
-    printf("\r\n small_driver_uart_init ok.");
     imu660ra_init();
     steer_control_init();              // 舵机控制初始化
-    printf("\r\n steer_control_init ok.");
     pit_ms_init(PIT_CH0,1);
-    
     
     // 此处编写用户代码 例如外设初始化代码等
     while(true)
     {
-        // 此处编写需要循环执行的代码
-        //if(system_time_state[9])   // 例如每 1ms 执行一次的代码
-        //{
-            //system_time_state[9] = 0;
-            ////printf("\r\n roll: %f, pit: %f, yaw: %f", roll_balance_cascade.posture_value.rol, roll_balance_cascade.posture_value.pit, roll_balance_cascade.posture_value.yaw);
-            //printf("%d,%d,%d\n",imu660ra_gyro_x, imu660ra_gyro_y, imu660ra_gyro_z);
-             //// 此处编写需要每 1ms 执行一次的代码
-            //// 此处编写需要每 1ms 执行一次的代码
-        //}
         printf("%d,%d,%d,%d,%d,%d\r\n",imu660ra_gyro_x, imu660ra_gyro_y, imu660ra_gyro_z,imu660ra_acc_x, imu660ra_acc_y, imu660ra_acc_z);
-        //printf("%f,%d,%d,%d,%d,%d\n",roll_balance_cascade.angular_speed_cycle.p,(int16)roll_balance_cascade.angular_speed_cycle.out,imu660ra_gyro_x,motor_value.receive_left_speed_data,motor_value.receive_right_speed_data);
-        //printf("%d,%d,%d\n",motor_value.receive_left_speed_data,motor_value.receive_right_speed_data,car_speed);
-        //small_driver_set_duty(250, 250);
         system_delay_ms(10);
-         // 此处编写需要每 1ms 执行一次的代码
-        // 此处编写需要循环执行的代码
     }
 }
-
-//void pit0_ch0_isr()                     // 定时器通道 0 周期中断服务函数      
-//{
-    //pit_isr_flag_clear(PIT_CH0);
-    //imu660ra_get_acc();
-    //imu660ra_get_gyro();
-//}
-// **************************** 代码区域 ****************************
